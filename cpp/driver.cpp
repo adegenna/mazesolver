ce046bd0d5f6fdde0899a915b74bad79d4dd772b
Anthony DeGennaro <adegennaro@bnl.gov>
2021-06-14 12:40:09 -0400

    Add new maze (but solver seg faults)

--- cpp/driver.cpp
+++ cpp/driver.cpp
@@ -6,19 +6,35 @@
 #include <istream>
 #include <random>
 #include <cmath>
-//#include <Eigen/Dense>
+#include <Eigen/Dense>
 //#include <boost/program_options.hpp>
 #include "utils.h"
 #include "Node.h"
+#include "search.h"
+#include "examplemaze.h"
 
 using namespace std;
 using namespace Eigen;
 
 int main(int argc, char* argv[]) {
-
-  printf("*********** DRIVER PROGRAM FOR MAZE SOLVER ***********\n\n");
   
-  Node( 5 , 7 );
+  printf("*********** DRIVER PROGRAM FOR MAZE SOLVER ***********\n\n");
+
+  MatrixXi M = make_maze_32x32();
+  
+  vector<vector<int>> exits = find_exit_points_of_maze( M );
+  
+  vector<int> ij_0 = exits[0];
+  vector<int> ij_f = exits[1];
+  
+  vector<Node> soln_nodes;
+  vector<vector<vector<int>>> soln_IJ;
+  
+  depth_first_solve( M ,
+		     ij_0 , ij_f ,
+		     soln_nodes , soln_IJ );
+  
+  
   
   return 0;
 }
---
+++ cpp/examplemaze.h
@@ -0,0 +1,8 @@
+#ifndef __EXAMPLE_MAZE_H__
+#define __EXAMPLE_MAZE_H__
+
+#include <Eigen/Dense>
+
+Eigen::MatrixXi make_maze_32x32( );
+
+#endif
