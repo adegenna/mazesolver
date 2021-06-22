a9f0c5701616dcade7f790dc240c1ffcfdbaf8e2
Anthony DeGennaro <amdegennaro@gmail.com>
2021-06-22 17:19:02 -0400

    Add dead end tracking and excepting to python solver

--- python/mazesolver.py
+++ python/mazesolver.py
@@ -10,6 +10,36 @@ from mazegenerator import *
 from utils import *
 
 
+def backtrack_and_mark_dead_end_nodes( stack_nodes , list_IJ ):
+    
+    dead_end_IJ = []
+    
+    stack_nodes[-1].nxt_index += 1
+    
+    if ( stack_nodes[-1].nxt_index >= len(stack_nodes[-1].valid_nxt_moves ) ):
+        dead_end_IJ.append( [ stack_nodes[-1].i , stack_nodes[-1].j ] )
+    
+    _ = list_IJ.pop()
+    list_IJ.append([])
+    
+    while(True):
+        
+        if ( stack_nodes[-1].nxt_index >= len(stack_nodes[-1].valid_nxt_moves) ):
+            
+            dead_end_IJ.append( [ stack_nodes[-1].i , stack_nodes[-1].j ] )
+            
+            _ = stack_nodes.pop()
+            stack_nodes[-1].nxt_index += 1
+            _ = list_IJ.pop()
+            _ = list_IJ.pop()
+            list_IJ.append([])
+        else:
+            break
+    
+    return stack_nodes , list_IJ , dead_end_IJ
+
+
+
 def backtrack( stack_nodes , list_IJ ):
     
     stack_nodes[-1].nxt_index += 1
@@ -37,7 +67,6 @@ def depth_first_solve( M , ij_0 , ij_f ):
     maze_nodes.append( Node( ij_0[0] , ij_0[1] ) )
     
     first_move = find_possible_moves( ij_0[0] , ij_0[1] , M )
-    #assert( len(first_move) == 1 )   # assuming entrance is not a split...
     first_move = first_move[0]
     
     ij = first_move
@@ -48,6 +77,7 @@ def depth_first_solve( M , ij_0 , ij_f ):
     IJ.append( [] )
     
     IJ_all = []
+    dead_end_IJ = []
     
     while( np.all(ij != ij_f) ):
         
@@ -56,7 +86,7 @@ def depth_first_solve( M , ij_0 , ij_f ):
                 
         if ( check_if_i0j0_is_maze_node( M , ij[0] , ij[1] , last_ij ) == True ):
 
-            if ( ij not in [ [mi.i , mi.j] for mi in maze_nodes ] ): # if you haven't been at this node before...
+            if ( (ij not in [ [mi.i , mi.j] for mi in maze_nodes ]) & (ij not in dead_end_IJ) ): # if you haven't been at this node before...
                 
                 maze_nodes.append( Node( ij[0] , ij[1] , last_ij ) )
                 
@@ -67,7 +97,9 @@ def depth_first_solve( M , ij_0 , ij_f ):
 
             else: # you have been at this node before, so it's a loop...
                 
-                maze_nodes , IJ = backtrack( maze_nodes , IJ )
+                #maze_nodes , IJ = backtrack( maze_nodes , IJ )
+                maze_nodes , IJ , dead_end_tmp = backtrack_and_mark_dead_end_nodes( maze_nodes , IJ )
+                dead_end_IJ += dead_end_tmp
                 
                 last_ij = [ maze_nodes[-1].i , maze_nodes[-1].j ]
                 ij      = maze_nodes[-1].calculate_valid_nxt_move( M )
@@ -84,7 +116,9 @@ def depth_first_solve( M , ij_0 , ij_f ):
                 
             elif ( len(moves) == 1 ): # dead end case
                 
-                maze_nodes , IJ = backtrack( maze_nodes , IJ )
+                #maze_nodes , IJ = backtrack( maze_nodes , IJ )
+                maze_nodes , IJ , dead_end_tmp = backtrack_and_mark_dead_end_nodes( maze_nodes , IJ )
+                dead_end_IJ += dead_end_tmp
                 
                 last_ij = [ maze_nodes[-1].i , maze_nodes[-1].j ]
                 ij      = maze_nodes[-1].calculate_valid_nxt_move( M )
@@ -120,7 +154,7 @@ def make_movie_of_maze_solve( M , IJ_all , entrance_ij , exit_ij , fig ):
         M_node[ exit_ij[0] , exit_ij[1] ] = 2
         frames.append( [ ax.imshow( M_node , vmin=0 , vmax=3 , cmap='afmhot') ] )
     
-    ani = animation.ArtistAnimation(fig, frames, interval=5, blit=True,
+    ani = animation.ArtistAnimation(fig, frames, interval=50, blit=True,
                                 repeat_delay=1000)
     ani.save('movie.mp4')
 
@@ -148,13 +182,13 @@ def plot_soln( M , IJ , entrance_ij , exit_ij , fig ):
 
 def main( M ):
     
-    #exits = find_exit_points_of_maze( M )
-    #assert( len(exits) == 2 ) # assume only 2 exit points of maze...
-    #entrance_ij = exits[0]
-    #exit_ij     = exits[1]
+    exits = find_exit_points_of_maze( M )
+    assert( len(exits) == 2 ) # assume only 2 exit points of maze...
+    entrance_ij = exits[0]
+    exit_ij     = exits[1]
     
-    entrance_ij = [ M.shape[0]//2 , 0 ]
-    exit_ij     = [ M.shape[0]//2 , M.shape[1]-1 ]
+    #entrance_ij = [ M.shape[0]//2 , 0 ]
+    #exit_ij     = [ M.shape[0]//2 , M.shape[1]-1 ]
     
     maze_nodes , IJ , IJ_all = depth_first_solve( M , entrance_ij , exit_ij )
     
@@ -162,9 +196,9 @@ def main( M ):
     print( 'solved path length: ' + str( np.sum( [ len(IJ[i]) for i in range(len(IJ)) ] ) ) )
     print( 'iterations taken: '   + str(np.shape( IJ_all )) )
     
-    #make_movie_of_maze_solve( -M+1 , IJ_all , entrance_ij , exit_ij , fig )
+    make_movie_of_maze_solve( -M+1 , IJ_all , entrance_ij , exit_ij , fig )
     
-    plot_soln( -M+1 , IJ , entrance_ij , exit_ij , fig )
+    #plot_soln( -M+1 , IJ , entrance_ij , exit_ij , fig )
     
     plt.show()
     
@@ -173,9 +207,9 @@ def main( M ):
     
 if __name__ == '__main__':
     
-    #main( make_test_maze_loops() )
+    main( make_test_maze_loops() )
     
-    M = easy_random_maze_gen( 32 , 128 )
-    plt.imshow( -M+1 , vmin=0 , vmax=3 , cmap='afmhot' )
-    plt.show()
-    main( M )
+    #M = easy_random_maze_gen( 32,128 )
+    #plt.imshow( -M+1 , vmin=0 , vmax=3 , cmap='afmhot' )
+    #plt.show()
+    #main( M )
