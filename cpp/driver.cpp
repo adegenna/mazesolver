af21fdee8f89b8c9790c7d83690f6443140f05ab
Anthony DeGennaro <adegennaro@bnl.gov>
2021-07-06 10:18:21 -0400

    Add example maze with loops

--- cpp/driver.cpp
+++ cpp/driver.cpp
@@ -19,7 +19,7 @@ int main(int argc, char* argv[]) {
   
   cout << "*********** DRIVER PROGRAM FOR MAZE SOLVER ***********\n\n";
   
-  MatrixXi M = make_maze_32x32();
+  MatrixXi M = make_maze_32x32_with_loops();
   
   vector<vector<int>> exits = find_exit_points_of_maze( M );
   
--- cpp/examplemaze.h
+++ cpp/examplemaze.h
@@ -5,6 +5,8 @@
 
 Eigen::MatrixXi make_maze_32x32( );
 
+Eigen::MatrixXi make_maze_32x32_with_loops( );
+
 // implement this function
 Eigen::MatrixXi translate_maze_to_adjacency_matrix(const Eigen::MatrixXi& maze);
 
