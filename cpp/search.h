3cf1618a0dc94f25af8be3856721e0fa5db4d5db
Mark Lohry <mlohry@gmail.com>
2021-07-08 17:01:44 -0400

    make private

--- cpp/search.h
+++ cpp/search.h
@@ -14,6 +14,7 @@ public:
 		const std::vector<int> &ij_0,
 		const std::vector<int> &ij_f) const;
 
+private:
 	std::vector<std::vector<int>> back_track_and_mark_dead_end_nodes(std::vector<Node> &stack_nodes,
 																	 std::vector<std::vector<std::vector<int>>> &list_IJ) const;
 };
